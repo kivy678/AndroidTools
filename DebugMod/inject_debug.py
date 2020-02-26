cc288c7ae679cde6bd244c3b06cfd44760115b53
kivy <kivy678@gmail.com>
2020-02-26 13:05:45 +0900

    commit

---
+++ DebugMod/inject_debug.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import os
+import shutil
+import shlex
+import subprocess
+
+from settings import *
+
+from bs4 import BeautifulSoup
+
+
+def readManifest():
+
+    try:
+        with open(MANIFEST) as fr:
+            soup = BeautifulSoup(fr, "lxml-xml")
+            soup.application['android:debuggable'] = 'true'
+
+            with open(MANIFEST_WRITE, "w", encoding='utf-8') as fw:
+                fw.write(str(soup))
+    except Exception as e:
+        print(e)
+        exit()
+
+
+def fileManger():
+
+    try:
+        os.remove(MANIFEST)
+        os.rename(MANIFEST_WRITE, MANIFEST)
+    except Exception as e:
+        print(e)
+        return False
+
+
+if __name__ == "__main__":
+
+    print("[*] start decode")
+    cmd = f"java -jar {APK_TOOL} d -f -o {DECODE_DIR} {TEST_APK}"
+    subprocess.call(shlex.split(cmd, posix=False))
+
+    readManifest()
+    fileManger()
+
+    print("[*] start build")
+    cmd = f"java -jar {APK_TOOL} b -f -o {OUT_APK} {DECODE_DIR}"
+    subprocess.call(shlex.split(cmd, posix=False))
+
+    print("[*] sign code")
+    cmd = f"java -jar {signapk} {certificate} {pk8} {OUT_APK} {KEY_APK}"
+    subprocess.call(shlex.split(cmd, posix=False))
+
+    print("[*] File Cleanup")
+    shutil.rmtree(DECODE_DIR, ignore_errors=True)
+    os.remove(OUT_APK)
+
+    print("Main End...")
