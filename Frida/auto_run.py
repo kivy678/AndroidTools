8e344240d63ad29d851aa1b2ea82027239bf6a3e
shw <shw@hauri.co.kr>
2020-04-28 16:33:05 +0900

    auto

--- Frida/auto_run.py
+++ Frida/auto_run.py
@@ -4,18 +4,47 @@ import os
 import subprocess
 import shlex
 
-s = shlex.shlex("adb root")
-s.whitespace_split = True
-subprocess.Popen(s, stdout=subprocess.PIPE, shell=True)
+subp = subprocess.Popen
 
-s = shlex.shlex("adb push " + r"TOOL\frida-server-12.7.15-android-arm" + " /data/local/tmp")
-s.whitespace_split = True
-subprocess.Popen(s, stdout=subprocess.PIPE, shell=True)
 
-s = shlex.shlex("adb shell chmod 755 /data/local/tmp/frida-server-12.7.15-android-arm")
-s.whitespace_split = True
-subprocess.Popen(s, stdout=subprocess.PIPE, shell=True)
+class ADBMODE(object):
+    _PLATFORM_ = {
+        "x86": "x86", "x64": "x64",
+        "armeabi_v7a": "arm", "arm64_v8a": "arm64"
+    }
 
-s = shlex.shlex("adb shell /data/local/tmp/frida-server-12.7.15-android-arm &")
-s.whitespace_split = True
-subprocess.Popen(s, stdout=subprocess.PIPE, shell=True)
+    def __getattr__(self, key):
+        try:
+            return self._PLATFORM_[key]
+        except KeyError as e:
+            return None
+
+    def __init__(self, *args, **kwargs):
+        subp(self.parseString("adb root"),
+             stdout=subprocess.PIPE)
+
+    def runCommand(self, cmd):
+        subp(self.parseString(cmd), stdout=subprocess.PIPE)
+
+    def parseString(self, cmd):
+        s = shlex.shlex(cmd)
+        s.whitespace_split = True
+
+        return s
+
+    def getSystem(self):
+        with subp(self.parseString("adb shell getprop ro.product.cpu.abi"), stdout=subprocess.PIPE) as proc:
+            stdin, stdout = proc.communicate()
+
+        return stdin.decode("utf-8").strip().replace('-', '_')
+
+
+adb_mod = ADBMODE()
+platform = getattr(adb_mod, adb_mod.getSystem())
+
+adb_mod.runCommand(
+    "adb push " + r"TOOL\frida-server-12.7.15-android-" + platform + " /data/local/tmp")
+adb_mod.runCommand(
+    "adb shell chmod 755 /data/local/tmp/frida-server-12.7.15-android-" + platform)
+adb_mod.runCommand(
+    "adb shell nohup /data/local/tmp/frida-server-12.7.15-android-" + platform + ' &')
