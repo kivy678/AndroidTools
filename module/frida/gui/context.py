6211df4f90e4d927b0f46087e8f9ba1c4ec6af3e
shw <shw@hauri.co.kr>
2020-10-07 18:51:01 +0900

    kivy 로그박스 수정박스로 나눔

---
+++ module/frida/gui/context.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+##################################################################################################
+
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.scrollview import ScrollView
+
+##################################################################################################
+
+
+class CONTEXT_BOX(ScrollView):
+    pass
+
+
+class CONTEXT_INPUT_BOX(ScrollView):
+    pass
+
+
+class CONTEXT_TAB_BOX(TabbedPanel):
+    pass
