b80d7673f13042ac304948d09626380180d55f94
kivy <kivy678@gmail.com>
2020-10-14 16:57:59 +0900

    elfformat 메모리누수 수정

--- module/Nlib/elfformat.cpp
+++ module/Nlib/elfformat.cpp
@@ -10,14 +10,84 @@
 #define PROGRAM_SIZE					3000
 #define SECTION_SIZE					3000
 #define DYNAMIC_SIZE					3000
-#define SYMTAB_SIZE						50000
+#define SYMTAB_SIZE						500000
 
 //////////////////////////////////////////////////////////////////////////////////
-
 
 PyObject* ElfFormatException = NULL;
 
 //////////////////////////////////////////////////////////////////////////////////
+
+
+int HeapFree(unsigned char* pchBuffer,
+			 char* pchEhrBuffer,
+			 char* pchPhrBuffer,
+			 char* pchShrBuffer,
+			 char* pchDynBuffer,
+			 char* pchSymBuffer,
+			 char* pchDsymBuffer,
+			 struct Elf32_Section_Linker* 	e32_section_linker,
+			 struct Elf32_Dyn_Linker* 		e32_dyn_linker,
+			 struct Elf32_Sym_Linker* 		e32_sym_linker,
+			 struct Elf32_Sym_Linker* 		e32_dsym_linker)
+{
+
+	struct Elf32_Section_Linker* 	NextNode1 = NULL;
+	struct Elf32_Dyn_Linker* 		NextNode2 = NULL;
+	struct Elf32_Sym_Linker* 		NextNode3 = NULL;
+
+	free(pchBuffer);
+
+	free(pchEhrBuffer);
+	free(pchPhrBuffer);
+	free(pchShrBuffer);
+	free(pchDynBuffer);
+	free(pchSymBuffer);
+	free(pchDsymBuffer);
+
+		
+	while(e32_section_linker->nextPoint != NULL)
+	{
+		NextNode1 = e32_section_linker->nextPoint;
+		free(e32_section_linker);
+
+		e32_section_linker = NextNode1;
+	}
+	free(e32_section_linker);
+
+
+	while(e32_dyn_linker->nextPoint != NULL)
+	{
+		NextNode2 = e32_dyn_linker->nextPoint;
+		free(e32_dyn_linker);
+
+		e32_dyn_linker = NextNode2;
+	}
+	free(e32_dyn_linker);
+
+
+	while(e32_sym_linker->nextPoint != NULL)
+	{
+		NextNode3 = e32_sym_linker->nextPoint;
+		free(e32_sym_linker);
+
+		e32_sym_linker = NextNode3;
+	}
+	free(e32_sym_linker);
+
+
+	while(e32_dsym_linker->nextPoint != NULL)
+	{
+		NextNode3 = e32_dsym_linker->nextPoint;
+		free(e32_dsym_linker);
+
+		e32_dsym_linker = NextNode3;
+	}
+	free(e32_dsym_linker);
+
+
+	return 0;
+}
 
 
 static PyObject *
@@ -26,6 +96,7 @@ parser(PyObject* self, PyObject* a_args)
 
 	//////////////////////////////////////////////////////////////////////////////////
 
+	PyObject* 		returnString = NULL;
 	FILE* 			pstRStream 	= NULL;
 	unsigned char* 	pchBuffer	= NULL;
 
@@ -98,17 +169,23 @@ parser(PyObject* self, PyObject* a_args)
 	{
 		ELFFORMAT_EXCEPTION(-1, "FILE OPEN FAILED!!!\n");
 
+		free(pchBuffer);
+
 		return 0;
 	}
 
 	//////////////////////////////////////////////////////////////////////////////////
 
 	EhrParser(pchBuffer, &e32_ehdr, pchEhrBuffer);
-	PhrParser(pchBuffer, &e32_ehdr, &e32_phdr, pchPhrBuffer);
-	ShrParser(pchBuffer, &e32_ehdr, e32_section_linker, pchShrBuffer);
-	DynParser(pchBuffer, &e32_ehdr, e32_dyn_linker, pchDynBuffer);
-	SymParser(pchBuffer, e32_section_linker, e32_sym_linker, pchSymBuffer, "SYMTAB");
-	SymParser(pchBuffer, e32_section_linker, e32_dsym_linker, pchDsymBuffer, "DYNSYM");
+
+	if (strcmp(OBJECT_FILE_CLASS[e32_ehdr.getFileClass()].c_str(), "ELF32") == 0)
+	{
+		PhrParser(pchBuffer, &e32_ehdr, &e32_phdr, pchPhrBuffer);
+		ShrParser(pchBuffer, &e32_ehdr, e32_section_linker, pchShrBuffer);
+		DynParser(pchBuffer, &e32_ehdr, e32_dyn_linker, pchDynBuffer);
+		SymParser(pchBuffer, e32_section_linker, e32_sym_linker, pchSymBuffer, "SYMTAB");
+		SymParser(pchBuffer, e32_section_linker, e32_dsym_linker, pchDsymBuffer, "DYNSYM");
+	}
 
 	//printf("%s\n", pchEhrBuffer);
 	//printf("%s\n", pchPhrBuffer);
@@ -117,70 +194,47 @@ parser(PyObject* self, PyObject* a_args)
 	//printf("%s\n", pchSymBuffer);
 	//printf("%s\n", pchDsymBuffer);
 
-	//free(pchEhrBuffer);
-	//free(pchPhrBuffer);
-	//free(pchShrBuffer);
-	//free(pchDynBuffer);
-	//free(pchSymBuffer);
-	//free(pchDsymBuffer);
-
-	//////////////////////////////////////////////////////////////////////////////////
-
-	free(pchBuffer);
-/*
-	struct Elf32_Section_Linker* NextNode = NULL;
-	while(e32_section_linker->nextPoint != NULL)
-	{
-		NextNode = e32_section_linker->nextPoint;
-		free(e32_section_linker);
-
-		e32_section_linker = NextNode;
-	}
-
-	struct Elf32_Dyn_Linker* NextNode = NULL;
-	while(e32_dyn_linker->nextPoint != NULL)
-	{
-		NextNode = e32_dyn_linker->nextPoint;
-		free(e32_dyn_linker);
-
-		e32_dyn_linker = NextNode;
-	}
-
-	struct Elf32_Sym_Linker* NextNode = NULL;
-	while(e32_sym_linker->nextPoint != NULL)
-	{
-		NextNode = e32_sym_linker->nextPoint;
-		free(e32_sym_linker);
-
-		e32_sym_linker = NextNode;
-	}
-*/
-
 	//////////////////////////////////////////////////////////////////////////////////
 
 	if (strcmp(option, "h") == 0)
-		return Py_BuildValue("s", pchEhrBuffer);
+		returnString = Py_BuildValue("s", pchEhrBuffer);
 
 	else if (strcmp(OBJECT_FILE_CLASS[e32_ehdr.getFileClass()].c_str(), "ELF64") == 0)
-		return Py_BuildValue("s", "NOT ELF32");
+		returnString = Py_BuildValue("s", pchEhrBuffer);
 
 	else if (strcmp(option, "p") == 0)
-		return Py_BuildValue("s", pchPhrBuffer);
+		returnString = Py_BuildValue("s", pchPhrBuffer);
 
 	else if (strcmp(option, "s") == 0)
-		return Py_BuildValue("s", pchShrBuffer);
+		returnString = Py_BuildValue("s", pchShrBuffer);
 
 	else if (strcmp(option, "d") == 0)
-		return Py_BuildValue("s", pchDynBuffer);
+		returnString = Py_BuildValue("s", pchDynBuffer);
 
 	else if (strcmp(option, "S") == 0)
-		return Py_BuildValue("s", pchSymBuffer);
+		returnString = Py_BuildValue("s", pchSymBuffer);
 
 	else if (strcmp(option, "dS") == 0)
-		return Py_BuildValue("s", pchDsymBuffer);
+		returnString = Py_BuildValue("s", pchDsymBuffer);
+
+	else
+		returnString = Py_BuildValue("sss", pchEhrBuffer, pchPhrBuffer, pchShrBuffer);
 
 
-	return Py_BuildValue("sss", pchEhrBuffer, pchPhrBuffer, pchShrBuffer);
+	HeapFree(pchBuffer,
+			 pchEhrBuffer,
+			 pchPhrBuffer,
+			 pchShrBuffer,
+			 pchDynBuffer,
+			 pchSymBuffer,
+			 pchDsymBuffer,
+			 e32_section_linker,
+			 e32_dyn_linker,
+			 e32_sym_linker,
+			 e32_dsym_linker);
+
+
+	return returnString;
 
 }
 
